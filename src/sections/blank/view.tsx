388bb1033ede024b050d3b64339bd40ca2cdc493
sagarhasan273 <sagarhasan273@gmail.com>
2024-12-18 12:34:42 +0600

    understanding about absolute

--- src/sections/blank/view.tsx
+++ src/sections/blank/view.tsx
@@ -19,12 +19,27 @@ export function BlankView({ title = 'Blank' }: Props) {
         sx={{
           mt: 5,
           width: 1,
-          height: 320,
+          height: 1,
           borderRadius: 2,
           bgcolor: (theme) => varAlpha(theme.vars.palette.grey['500Channel'], 0.04),
           border: (theme) => `dashed 1px ${theme.vars.palette.divider}`,
         }}
-      />
+      >
+        <Box sx={{ bgcolor: 'red', height: 50, width: 50, color: 'white' }}>box 1</Box>
+        <Box sx={{ bgcolor: 'green', height: 50, width: 50, color: 'white' }}>box 2</Box>
+        <Box
+          sx={{
+            bgcolor: 'black',
+            height: 50,
+            width: 50,
+            color: 'white',
+            position: 'absolute',
+            top: 0,
+          }}
+        >
+          box 3
+        </Box>
+      </Box>
     </DashboardContent>
   );
 }
